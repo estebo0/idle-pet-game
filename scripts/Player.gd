a54393d1dde87b5ecb7ab8a992dacf1b9dfcdc73
estebo0 <estebocm@gmail.com>
2026-06-20 20:12:43 +0200

    Switch to top-down view (Tibia style): green map, player moves horizontally

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,6 +5,7 @@ const SPEED = 150.0
 func _process(delta: float) -> void:
 	position.x += SPEED * delta
 
+	# Al salir por la derecha vuelve por la izquierda
 	var screen_width = get_viewport_rect().size.x
 	if position.x > screen_width + 50:
 		position.x = -50
