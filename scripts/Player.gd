a5795b4506d00c3190560d9cbc67d1da55da9aca
estebo0 <estebocm@gmail.com>
2026-06-20 20:09:21 +0200

    Fix scene files: use Node2D to avoid undefined collision shapes

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,10 @@
-extends CharacterBody2D
+extends Node2D
 
 const SPEED = 150.0
 
-func _physics_process(delta: float) -> void:
-	velocity.x = SPEED
-	move_and_slide()
+func _process(delta: float) -> void:
+	position.x += SPEED * delta
 
-	# Vuelve al inicio cuando sale por la derecha de la pantalla
 	var screen_width = get_viewport_rect().size.x
 	if position.x > screen_width + 50:
 		position.x = -50
